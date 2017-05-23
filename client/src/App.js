a7bc925017d1b6097fc46c562a3fb8e3e1ff8f85
david spencer <dspencer926@gmail.com>
2017-05-23 12:36:14 -0400

    moved responsivevoice file and translation file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import responsiveVoice from './responsiveVoice.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.speak = this.speak.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({text: e.target.value});
+  }
+
+  speak(e) {
+    e.preventDefault();
+    let text = this.state.text;
+    responsiveVoice.speak(this.state.text, 'Russian Female', {rate: 1.0});
+    // responsiveVoice.speak(this.state.text, 'Russian Female');
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.speak}>
+          <input type='text' name='text' onChange={this.handleInput}/>
+          <button onClick={this.speak}>submit</button>
+        </form>
       </div>
     );
   }
