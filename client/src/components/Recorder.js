e02666e6abeaa4547ba16e752d84defee9b46a2a
david spencer <dspencer926@gmail.com>
2017-05-25 15:43:10 -0400

    working on audio

--- client/src/components/Recorder.js
+++ client/src/components/Recorder.js
@@ -9,15 +9,22 @@ class Nav extends Component {
     super(props);
     this.state = {
       mediaWorks: false,
+      audioClip: null,
     }
+    this.audioProcess = this.audioProcess.bind(this);
   }
 
 
 
   componentDidMount() {
+    this.audioProcess();
+  }
+
+  audioProcess() {
     const record = document.querySelector('#record');
     const stop = document.querySelector('#stop');
     const soundClips = document.querySelector('#sound-clips');
+    let audioClip = null;
     if (navigator.getUserMedia) {
       console.log('getUserMedia supported.');
       navigator.getUserMedia (
@@ -31,19 +38,65 @@ class Nav extends Component {
         record.style.background = "red";
         record.style.color = "black";
       }
+      let chunks = [];
+      mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data)};
+      stop.onclick = function() {
+        if(mediaRecorder.state === 'recording') {
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+        record.style.background = "";
+        record.style.color = "";
+        } else {
+          console.log('not recording!');
+        }
+      }
+      mediaRecorder.onstop = function(e) {
+        console.log("recorder stopped");
 
-        
-      },
+        let clipName = prompt('Enter a name for your sound clip');
+
+        let clipContainer = document.createElement('article');
+        let clipLabel = document.createElement('p');
+        let audio = document.createElement('audio');
+        let deleteButton = document.createElement('button');
+                
+        clipContainer.classList.add('clip');
+        audio.setAttribute('controls', '');
+        deleteButton.innerHTML = "Delete";
+        clipLabel.innerHTML = clipName;
+
+        clipContainer.appendChild(audio);
+        clipContainer.appendChild(clipLabel);
+        clipContainer.appendChild(deleteButton);
+        soundClips.appendChild(clipContainer);
+
+        let blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+        audioClip = blob;
+        chunks = [];
+        console.log(blob);
+        let audioURL = window.URL.createObjectURL(blob);
+        audio.src = audioURL;
+        console.log(audioURL);
+        deleteButton.onclick = function(e) {
+          let evtTgt = e.target;
+          evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+        }
+      }},
       function(err) {           // Error callback
          console.log('The following gUM error occured: ' + err);
-      }
-   );
-} else {
-   console.log('getUserMedia not supported on your browser!');
-}
+      });
+    } else {
+      console.log('getUserMedia not supported on your browser!');
+    }
+
   }
 
 
+      audioTest(msg) {
+        console.log('in audio test');
+      }
 
 
   render() {
@@ -52,6 +105,7 @@ class Nav extends Component {
         <button id='record'>Record</button>
         <button id='stop'>Stop</button>
         <button id='playback'>Play</button>
+        <div id='sound-clips'></div>
       </div>
 
     );
