e1ee2a71a179b5aee8a6e2a2d68765069b8726e4
david spencer <dspencer926@gmail.com>
2017-05-25 10:59:31 -0400

    trying to get fix setState

--- client/src/components/Translation.js
+++ client/src/components/Translation.js
@@ -6,7 +6,9 @@ class Translation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      langFrom: '',
+      langTo: null,
     }
     this.handleInput = this.handleInput.bind(this);
     this.speak = this.speak.bind(this);
@@ -15,6 +17,11 @@ class Translation extends Component {
 
 translation(e) {
   e.preventDefault();
+  console.log(e.target.langTo.value);
+  console.log('setting state');
+  this.setState({langTo: e.target.langTo.value});
+  console.log('state set');
+  console.log(this.state);
   fetch('http://localhost:3001/translation', {
     method: 'POST',
     headers: {
@@ -34,22 +41,53 @@ translation(e) {
   })
 }
 
-  handleInput(e){
+  handleInput(e) {
     this.setState({text: e.target.value});
   }
 
   speak(data) {
     console.log(data)
-    responsiveVoice.speak(data.translation, 'Spanish Female', {rate: 1.0});
+    responsiveVoice.speak(data.translation, 'Spanish Male', {rate: 1.0});
   }
 
   render() {
+    console.log('render');
     return (
       <div id='translation-container'>
         <div id='translation-div'>
           <div id='input-div'>
-            <form onSubmit={this.translation}>
-              <textarea name='text' rows='4' onChange={this.handleInput}/>
+            <form onSubmit={(e) => this.translation(e)}>
+              <textarea name='text' rows='4' onChange={this.handleInput}/><br/>
+                <div>Translate from:</div>
+                <select name='langFrom'> 
+                  <option value='en'>English</option>
+                  <option value='es'>Spanish</option>
+                  <option value='fr'>French</option>
+                  <option value='pt'>Portuguese</option>
+                  <option value='it'>Italian</option>
+                  <option value='ru'>Russian</option>
+                  <option value='ar'>Arabic</option>
+                  <option value='zh-CN'>Chinese</option>
+                  <option value='ja'>Japanese</option>
+                  <option value='de'>German</option>
+                  <option value='sw'>Swahili</option>
+                  <option value='iw'>Hebrew</option>
+                </select><br/>
+                <div>Translate to:</div>
+                <select name='langTo'> 
+                  <option value='en'>English</option>
+                  <option value='es'>Spanish</option>
+                  <option value='fr'>French</option>
+                  <option value='pt'>Portuguese</option>
+                  <option value='it'>Italian</option>
+                  <option value='ru'>Russian</option>
+                  <option value='ar'>Arabic</option>
+                  <option value='zh-CN'>Chinese</option>
+                  <option value='ja'>Japanese</option>
+                  <option value='de'>German</option>
+                  <option value='sw'>Swahili</option>
+                  <option value='iw'>Hebrew</option>
+                </select><br/>
               <textarea name='lang' rows='4' id='language-box' />
               <input type='submit'/>
             </form>
